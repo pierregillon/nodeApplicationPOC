8890c5f539c52f647ee11f4662d7676947515b47
Pierre GILLON <gillon.pierre69@gmail.com>
2015-09-05 02:13:16 +0200

    Calculate key of routehandler from GetPath() to render animation

--- src/shared/components/template.react.jsx
+++ src/shared/components/template.react.jsx
@@ -3,21 +3,23 @@
     module.exports = Template;
 
     var React = require('react/addons');
+    var ReactRouter = require('react-router');
     var RouteHandler = require('react-router').RouteHandler;
     var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 
     function Template(SiteMap, Header){
         return React.createClass({
+            mixins: [ ReactRouter.State ],
             render : function() {
-                var key = "";
+                var key = this.getPath();
                 return (
                     <div>
                         <Header />
                         <SiteMap />
                         <div className="content">
                             <ReactCSSTransitionGroup transitionName="pageTransition" transitionLeave={false}>
-                                <RouteHandler key={key}/>
+                                    <RouteHandler key={key}/>
                             </ReactCSSTransitionGroup>
                         </div>
                     </div>
